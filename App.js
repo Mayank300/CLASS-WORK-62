7d0382e4f4c0d05e35efef21bbe304da1219c1eb
Mayank_Ratre <mratre300@gmail.com>
2021-01-07 20:46:04 +0530

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
+export default class WeatherScreen extends Component {
+constructor(){
+  super();
+    this.state = {
+      weather: '',
+    };
+}
+
+getWeather = async () =>{
+  var url = 'https://fcc-weather-api.glitch.me/api/current?lat=15.058800&lon=76.595398'
+  return fetch(url)
+  .then(response => response.json())
+  .then(responseJson => {
+    this.setState({
+      weather: responseJson,
+    });
+  })
+
+  .catch(error => {
+    console.error(error);
+    });
+
+};
+
+componentDidMount = () =>{
+  this.getWeather();
+}
+
+render(){
+  if(this.state.weather === ''){
+    
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>LOADING...</Text>
       <StatusBar style="auto" />
     </View>
   );
+  } else{
+      return(
+        <View style={styles.container}>
+        <View style={styles.subContainer}>
+          <Text style={styles.title}>
+            Weather Forecast
+          </Text>
+          <Image
+            style={styles.cloudImage}
+            source={require('./cloud.png')}
+          />
+          <View style={styles.textContainer}>
+          <Text style={{ fontSize: 18}}>
+            {this.state.weather.main.temp}&deg;C
+          </Text>
+          <Text style={{ fontSize: 20, margin:10}}>
+            humidity : {this.state.weather.main.humidity}
+          </Text>
+          <Text style={{fontSize: 20}}>
+            {this.state.weather.weather[0].description}
+          </Text>
+        </View>
+        </View>    
+      </View>
+      )
+  }
 }
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex:1
   },
+  subContainer : { 
+    flex: 1, 
+    borderWidth: 1, 
+    alignItems: 'center' 
+    },
+    title:{ 
+      marginTop: 50, 
+      fontSize: 30,
+      fontWeight: '550' 
+    },
+    cloudImage :{ 
+      width: 200, 
+      height: 200, 
+      marginTop: 30 
+    },
+    textContainer : { 
+      flex: 1,
+      alignItems: 'center', 
+      flexDirection:'row', 
+      marginTop:-150
+    }
 });
